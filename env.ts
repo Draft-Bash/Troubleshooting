64861196761b466f035ad4819730e44560bc17ca
Stephen Feddes <stephenpfeddes@lewisu.edu>
2023-09-15 21:22:26 -0500

    Changing yml file for CI/CD.

--- env.ts
+++ env.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://draftbash.azurewebsites.net/api/v1"
-export const SERVER_URL = "https://draftbash.azurewebsites.net"
+export const API_URL = "http://localhost:3000/api/v1"
+export const SERVER_URL = "http://localhost:3000"
